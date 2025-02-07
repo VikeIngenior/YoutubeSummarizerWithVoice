a66a0cd3b712f0ed44ad70f6bfeacabab4cd90ff
Erencan Ekinci <kyse1234@hotmail.com>
2025-02-07 19:26:06 +0300

    fix wrong os usage

--- voiceover.py
+++ voiceover.py
@@ -3,10 +3,6 @@ import os
 from openai import OpenAI, OpenAIError
 
 api_key = os.getenv("OPENAI_API_KEY")
-# if not api_key:
-#     raise ValueError("OPENAI_API_KEY is not set. Please provide a valid API key.")
-# else:
-#     client = OpenAI(api_key=api_key)
 
 if api_key:
     client = OpenAI(api_key=api_key)
